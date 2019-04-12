a7c51c1c083b845baa4e6ce2915f4b6f60d8151d
cjwood032 <cjwood032@gmail.com>
2019-04-12 15:53:05 -0400

    building out scraper

--- lib/method_gem/nokogiri.rb
+++ lib/method_gem/nokogiri.rb
@@ -1,9 +1,24 @@
 class MethodGem::Scraper
 
     def scrape_page(type)
-        @main=Nokogiri::HTML(open("https://ruby-doc.org/core-2.5.3/#{url}.html"))
+        @main=Nokogiri::HTML(open("https://ruby-doc.org/core-2.5.3/#{type}.html"))
     end
-    def get names
+    def get_names
         @names = @main.css(".wrapper").css(".method-callseq").map {|e| e.children.text.split[0]}.uniq.map{|n| n.split('(')[0]}
     end
+    def get_def(num)
+        o[num].text.split.join(' ')
+    end
+    def get_description
+        @desc=@main.css(".wrapper").css("#description").text
+    end
 end
+everything=@main.css(".wrapper").text.split
+i=everything.find_index("Public")
+if everything[i+1]=="Class" && everything[i+2]=="Method"
+    cut=everything.drop(i+3)
+end
+#have a loop over ther entirety
+# find the click to toggle 
+# everything before the click to toggle will be the name
+#everything after up to a point will be the description
